7a5b5f65c34667435d819dc722bf4ae6b1082b46
Tanvi Patil <tp1326460@gmail.com>
2025-07-06 18:47:42 +0530

    Update app.py

--- app.py
+++ app.py
@@ -40,4 +40,5 @@ def faq():
     return render_template("faq.html")
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
